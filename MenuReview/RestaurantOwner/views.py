c4fa0d805cbd57f5f41cf15652b2ef519242c85d
zexigong <108034351+zexigong@users.noreply.github.com>
2023-04-12 15:37:31 -0700

    api completed for restaurant, course and dish models

--- MenuReview/RestaurantOwner/views.py
+++ MenuReview/RestaurantOwner/views.py
@@ -33,3 +33,58 @@ def restaurantApi(request,id=0):
         restaurant = Restaurants.objects.get(RestaurantId=id)
         restaurant.delete()
         return JsonResponse('Deleted Successfully',safe=False)
+    
+
+@csrf_exempt
+def courseApi(request,id=0):
+    if request.method == 'GET':
+        courses = Courses.objects.all()
+        courses_serializer=CoursesSerializer(courses,many=True)
+        return JsonResponse(courses_serializer.data,safe=False)
+    elif request.method == 'POST':
+        courses_data = JSONParser().parse(request)
+        courses_serializer=CoursesSerializer(data=courses_data)
+        if courses_serializer.is_valid():
+            courses_serializer.save()
+            return JsonResponse('Added Successfully', safe=False)
+        return JsonResponse('Failed to Add',safe=False)
+    elif request.method == 'PUT':
+        courses_data = JSONParser().parse(request)
+        courses = Courses.objects.get(CourseId=courses_data['CourseId'])
+        courses_serializer=CoursesSerializer(courses,data=courses_data)
+        if courses_serializer.is_valid():
+            courses_serializer.save()
+            return JsonResponse('Updated Successfully', safe=False)
+        return JsonResponse('Failed to Update',safe=False)
+    elif request.method == 'DELETE':
+        print(id)
+        courses = Courses.objects.get(CourseId=id)
+        courses.delete()
+        return JsonResponse('Deleted Successfully',safe=False)
+    
+
+@csrf_exempt
+def dishApi(request,id=0):
+    if request.method == 'GET':
+        dishes = Dishes.objects.all()
+        dishes_serializer=DishesSerializer(dishes,many=True)
+        return JsonResponse(dishes_serializer.data,safe=False)
+    elif request.method == 'POST':
+        dishes_data = JSONParser().parse(request)
+        dishes_serializer=DishesSerializer(data=dishes_data)
+        if dishes_serializer.is_valid():
+            dishes_serializer.save()
+            return JsonResponse('Added Successfully', safe=False)
+        return JsonResponse('Failed to Add',safe=False)
+    elif request.method == 'PUT':
+        dishes_data = JSONParser().parse(request)
+        dishes = Dishes.objects.get(DishId=dishes_data['DishId'])
+        dishes_serializer=DishesSerializer(dishes,data=dishes_data)
+        if dishes_serializer.is_valid():
+            dishes_serializer.save()
+            return JsonResponse('Updated Successfully', safe=False)
+        return JsonResponse('Failed to Update',safe=False)
+    elif request.method == 'DELETE':
+        dishes = Dishes.objects.get(DishId=id)
+        dishes.delete()
+        return JsonResponse('Deleted Successfully',safe=False)
