61c0ab96d418f18582578f2aa60f93223709fd5d
zexigong <108034351+zexigong@users.noreply.github.com>
2023-04-23 19:43:16 -0700

    add comments for test

--- MenuReview/RestaurantOwner/tests.py
+++ MenuReview/RestaurantOwner/tests.py
@@ -11,10 +11,12 @@ class RestaurantViewGetTest(TestCase):
         creat_restaurant_objects(3)
 
     def test_view_url_exists_at_desired_location(self):
+        # test if the url works
         response = self.client.get('/restaurant')
         self.assertEqual(response.status_code, 200)
 
     def test_default_get_method(self):
+        # test the get method with default setting
         response = self.client.get('/restaurant')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.json()['data']), 3)
@@ -26,26 +28,25 @@ class RestaurantViewGetTest(TestCase):
         self.assertEqual(len(response.json()['data']), 1)
 
     def test_restaurent_sortby_name(self):
+        # test the get and sort by RestaurantName
         response = self.client.get('/restaurant' + '?sortBy=RestaurantName')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json()['data'][0]["RestaurantName"], 'a 0')
 
     def test_restaurent_sortby_name_desc(self):
+        # test the get and sort by RestaurantName in descending order
         response = self.client.get('/restaurant' + '?sortBy=RestaurantName&sortDesc=true')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json()['data'][0]["RestaurantName"], 'a 2')
 
-    def test_restaurent_sortby_id_desc(self):
-        response = self.client.get('/restaurant' + '?sortBy=RestaurantId&sortDesc=true')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json()['data'][0]["RestaurantName"], 'a 2')
-
     def test_restaurent_search1(self):
+        # test the get and search number method
         response = self.client.get('/restaurant' + '?search=2')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.json()['data']), 1)
 
     def test_restaurent_search2(self):
+        # test the get and search character method
         response = self.client.get('/restaurant' + '?search=c')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.json()['data']), 0)
@@ -59,12 +60,14 @@ class RestaurantViewPostTest(TestCase):
         pass
         
     def test_post_successed_method(self):
+        # test the post method with valid input
         data={"RestaurantName": "Mourad","RestaurantIntro": "hoa cai"}
         response = self.client.post('/restaurant', data, content_type='application/json')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content, b'"Added Successfully"')
 
     def test_post_failed_method(self):
+        # test the post method with invalid input
         data={"RestaurantName": "","RestaurantIntro": "hoa cai"}
         response = self.client.post('/restaurant', data, content_type='application/json')
         self.assertEqual(response.status_code, 200)
@@ -78,6 +81,7 @@ class RestaurantViewPutTest(TestCase):
         creat_restaurant_objects(1)
 
     def test_put_succeed_method(self):
+        # test the put method with valid input
         restaurant_id = int(self.client.get('/restaurant').json()['data'][0]["RestaurantId"])
         data={"RestaurantId": restaurant_id,"RestaurantName": "put_test","RestaurantIntro": "test"}
         response = self.client.put('/restaurant', data, content_type='application/json')
@@ -85,6 +89,7 @@ class RestaurantViewPutTest(TestCase):
         self.assertEqual(response.content, b'"Updated Successfully"')
     
     def test_put_failed_method(self):
+        # test the put method with invalid input
         restaurant_id = int(self.client.get('/restaurant').json()['data'][0]["RestaurantId"])
         data={"RestaurantId": restaurant_id,"RestaurantName": "","RestaurantIntro": "test"}
         response = self.client.put('/restaurant', data, content_type='application/json')
@@ -98,7 +103,8 @@ class RestaurantViewDeleteTest(TestCase):
         # Create 3 restaurant for delete tests
         creat_restaurant_objects(3)
 
-    def test_delete_failed_method(self):
+    def test_delete_method(self):
+        # test the delete method
         restaurant_id = str(self.client.get('/restaurant').json()['data'][0]["RestaurantId"])
         response = self.client.delete('/restaurant/' + restaurant_id)
         self.assertEqual(response.status_code, 200)
@@ -112,10 +118,12 @@ class DishViewGetTest(TestCase):
         creat_dishes_objects(12)
 
     def test_view_url_exists_at_desired_location(self):
+        # test if the url works
         response = self.client.get('/dish')
         self.assertEqual(response.status_code, 200)
 
     def test_default_get_method(self):
+        # test the get method with default setting
         response = self.client.get('/dish')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.json()['data']), 12)
@@ -127,31 +135,37 @@ class DishViewGetTest(TestCase):
         self.assertEqual(len(response.json()['data']), 2)
 
     def test_dish_sortby_name(self):
+        # test the get and sort by DishName
         response = self.client.get('/dish' + '?sortBy=DishName')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json()['data'][0]["DishName"], 'a 0')
 
     def test_dish_sortby_name_desc(self):
+        # test the get and sort by DishName in descending order
         response = self.client.get('/dish' + '?sortBy=DishName&sortDesc=true')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json()['data'][0]["DishName"], 'a 9')
 
     def test_dish_sortby_rating_desc(self):
+        # test the get and sort by Rating in descending order
         response = self.client.get('/dish' + '?sortBy=Rating&sortDesc=true')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json()['data'][0]["Rating"], 5)
 
     def test_dish_search_word(self):
+        # test the get and search string method
         response = self.client.get('/dish' + '?search=1')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.json()['data']), 3)
 
     def test_dish_search_price(self):
+        # test the get and search price range method
         response = self.client.get('/dish' + '?lowPrice=0&highPrice=5')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.json()['data']), 6)
 
     def test_dish_search_rate(self):
+        # test the get and search rating range method
         response = self.client.get('/dish' + '?lowRate=4&highRate=5')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.json()['data']), 4)
@@ -165,14 +179,14 @@ class DishViewPostTest(TestCase):
         pass
         
     def test_post_successed_method(self):
+        # test the post method with valid input
         data={"DishName": "Shortrib","Restaurant": "Mourad","Taste": "Salty","Price":160,"Rating":5}
-        # print(self.client.json_encoder)
-        # self.client.json_encoder = None
         response = self.client.post('/dish', data, content_type='application/json')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content, b'"Added Successfully"')
 
     def test_post_failed_method(self):
+        # test the post method with invalid input
         data={"DishName": "","Restaurant": "Mourad","Taste": "Salty","Price":160,"Rating":5}
         response = self.client.post('/dish', data, content_type='application/json')
         self.assertEqual(response.status_code, 200)
@@ -186,6 +200,7 @@ class DishViewPutTest(TestCase):
         creat_dishes_objects(1)
 
     def test_put_succeed_method(self):
+        # test the put method with valid input
         dish_id = int(self.client.get('/dish').json()['data'][0]["DishId"])
         data={"DishId": dish_id,"DishName": "Shortrib","Restaurant": "Mourad","Taste": "Salty","Price":160,"Rating":5}
         response = self.client.put('/dish', data, content_type='application/json')
@@ -193,6 +208,7 @@ class DishViewPutTest(TestCase):
         self.assertEqual(response.content, b'"Updated Successfully"')
     
     def test_put_failed_method(self):
+        # test the put method with invalid input
         dish_id = int(self.client.get('/dish').json()['data'][0]["DishId"])
         data={"DishId": dish_id,"DishName": "","Restaurant": "Mourad","Taste": "Salty","Price":160,"Rating":5}
         response = self.client.put('/dish', data, content_type='application/json')
@@ -206,7 +222,8 @@ class DishViewDeleteTest(TestCase):
         # Create 1 dishes for delete tests
         creat_dishes_objects(1)
 
-    def test_delete_failed_method(self):
+    def test_delete_method(self):
+        # test the delete method
         dish_id = int(self.client.get('/dish').json()['data'][0]["DishId"])
         response = self.client.delete('/dish/' + str(dish_id))
         self.assertEqual(response.status_code, 200)
